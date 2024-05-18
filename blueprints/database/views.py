71deed92edd0d8623f9f44caef8093172b3b65ed
Meagan <welchmea@oregonstate.edu>
2024-05-17 23:54:33 -0500

    reverted to previous commit

--- blueprints/database/views.py
+++ blueprints/database/views.py
@@ -4,10 +4,9 @@ import certifi
 import os
 
 ENV = 'LIVE'
-if ENV == 'DEV':
+if ENV =='DEV':
 # sets ups access to MongoDB
     mongo_db_url = os.environ.get("MONGO_DB_CONN_STRING")
-
 if ENV == 'LIVE':
     mongo_db_url = os.environ.get('MONGODB_URI')
     
